506cff153bd4b42641a727b498f1c2ff8e23ee17
LcrPrograming <83461083+Luiscrd@users.noreply.github.com>
2022-11-04 08:59:49 +0100

    Upload

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,15 +2,15 @@
     <div class="container mt-5">
         <h1>Counter - Vuex</h1>
         <hr>
-        <h2>Aceso directo <small>($store.state.count)</small>:  {{ $store.state.count }}</h2>
+        <h2>Aceso directo <small>($store.state.count)</small>: {{ $store.state.count }}</h2>
         <hr>
         <h2>Computada <small>(countCoumputed() { return this.$store.state.count })</small>: {{ countCoumputed }}</h2>
         <hr>
         <h1>MapState</h1>
         <hr>
-        <button type="button" class="btn btn-primary m-2" @click="increment">+1</button>
-        <button type="button" class="btn btn-primary m-2" @click="incrementBy">+5</button>
-        <button type="button" class="btn btn-primary m-2" @click="incrementRandomInt">Random</button>
+        <button type="button" class="btn btn-primary m-2" @click="increment" :disabled="isLoading">+1</button>
+        <button type="button" class="btn btn-primary m-2" @click="incrementBy" :disabled="isLoading">+5</button>
+        <button type="button" class="btn btn-primary m-2" @click="incrementRandomInt" :disabled="isLoading">Random</button>
         <hr>
         <h2>count: {{ count }}</h2>
         <hr>
@@ -27,11 +27,12 @@ export default {
         countCoumputed() {
             return this.$store.state.count
         },
-        // ...mapState(['count', 'lastMutation'])
-        ...mapState({
-            count: state => state.count,
-            lastMutation: state => state.lastMutation
-        })
+        ...mapState(['count', 'lastMutation','isLoading'])
+        // ...mapState({
+        //     count: state => state.count,
+        //     lastMutation: state => state.lastMutation,
+        //     isLoading: state => state.isLoading
+        // })
     },
     methods: {
         increment() {
@@ -52,8 +53,9 @@ export default {
 </script>
 
 <style scoped>
-
-hr, h1, h2{
+hr,
+h1,
+h2 {
     color: #394A5C;
 }
 
@@ -62,6 +64,7 @@ hr, h1, h2{
     background-color: #232d38;
     border-color: #232d38;
 }
+
 .btn-primary {
     --bs-btn-color: #fff;
     --bs-btn-bg: #394A5C;
@@ -69,7 +72,7 @@ hr, h1, h2{
     --bs-btn-hover-color: #fff;
     --bs-btn-hover-bg: #394A5C;
     --bs-btn-hover-border-color: #394A5C;
-    --bs-btn-focus-shadow-rgb: 49,132,253;
+    --bs-btn-focus-shadow-rgb: 49, 132, 253;
     --bs-btn-active-color: #fff;
     --bs-btn-active-bg: #394A5C;
     --bs-btn-active-border-color: #394A5C;
@@ -78,5 +81,4 @@ hr, h1, h2{
     --bs-btn-disabled-bg: #394A5C;
     --bs-btn-disabled-border-color: #394A5C;
 }
-
 </style>
